d3b87258c0828ee3e739ba96dc7c5ff775be1087
Morgan Slater <morgan.slater@oneiota.co.uk>
2022-09-27 13:12:32 +0100

    rollback BuildServiceError changes

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/slatermorgan/lambda-handlers
 go 1.18
 
 require (
+	bitbucket.org/oneiota/serviceerror v1.3.0
 	github.com/aws/aws-lambda-go v1.34.1
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.7.0
--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+
+	"bitbucket.org/oneiota/serviceerror"
 )
 
 // Genertic Handler object which is the reciever in every handler method
@@ -48,19 +50,21 @@ func (r *ResponseHandler) BuildResponder(code int, body string) (Responder, erro
 
 func (r *ResponseHandler) BuildErrorResponse(err error) (Responder, error) {
 	statusCode := http.StatusInternalServerError
+	var serviceErr *serviceerror.ServiceError
 
-	type statusCoder interface {
-		StatusCode() int
-	}
-
-	servErr, hasStatusCode := err.(statusCoder)
-	if hasStatusCode {
-		statusCode = servErr.StatusCode()
+	switch err := err.(type) {
+	case *serviceerror.ServiceError:
+		statusCode = err.StatusCode()
+		serviceErr = err
+	default:
+		// If its a general error - we don't want to return the message as its a code/integration issue.
+		// We don't want those messages being shown to users.
+		serviceErr = serviceerror.Unknown("An unknown error occurred")
 	}
 
 	if statusCode == http.StatusInternalServerError {
 		r.logger.Error(err)
 	}
 
-	return r.BuildResponse(statusCode, err)
+	return r.BuildResponse(statusCode, serviceErr)
 }
